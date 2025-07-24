4ffe7cdf3b663ab171b485e18d812a81e756b209
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-24 21:51:10 +0600

    Add Mwnvgsi component

---
+++ src/components/Mwnvgsi.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mwnvgsi = () => {
+  return (
+    <div>
+      <h2>Mwnvgsi says hi 👋</h2>
+      <p>Dolor 74</p>
+    </div>
+  );
+}
+
+export default Mwnvgsi;
