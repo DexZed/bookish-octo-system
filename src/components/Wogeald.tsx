56a8d13c84178a133fb4fbb160e6468adf80f981
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-24 21:51:10 +0600

    Add Wogeald component

---
+++ src/components/Wogeald.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Wogeald = () => {
+  return (
+    <div>
+      <h2>Wogeald says hi 👋</h2>
+      <p>Dolor 69</p>
+    </div>
+  );
+}
+
+export default Wogeald;
