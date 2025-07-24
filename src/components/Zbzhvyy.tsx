09f68d2e9c76fc334711f3ddeff6747e9351973c
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-24 21:51:10 +0600

    Add Zbzhvyy component

---
+++ src/components/Zbzhvyy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Zbzhvyy = () => {
+  return (
+    <div>
+      <h2>Zbzhvyy says hi 👋</h2>
+      <p>Dolor 45</p>
+    </div>
+  );
+}
+
+export default Zbzhvyy;
