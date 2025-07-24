f39462733afeda60ad118f8217b1fe0d4cf60239
DexZed <144045080+DexZed@users.noreply.github.com>
2025-07-24 21:51:10 +0600

    Add Rnqmdhm component

---
+++ src/components/Rnqmdhm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rnqmdhm = () => {
+  return (
+    <div>
+      <h2>Rnqmdhm says hi 👋</h2>
+      <p>Lorem 88</p>
+    </div>
+  );
+}
+
+export default Rnqmdhm;
